9821cc87e0684ca02a388650187474621ccf5ffc
benjrkeith <37029865+benjrkeith@users.noreply.github.com>
2025-02-25 12:37:48 +0000

    Add 'no users' and 'x more users' labels, only show first n users.

--- client/src/ui/misc/UserList/UserList.tsx
+++ client/src/ui/misc/UserList/UserList.tsx
@@ -2,6 +2,8 @@ import UserListEntry, { UserListAction } from '@/ui/misc/UserList/UserListEntry'
 
 import * as types from '@/types'
 
+const NUM_TO_SHOW = 20
+
 type UserListProps = {
   users: types.User[]
   action?: UserListAction
@@ -10,9 +12,17 @@ type UserListProps = {
 export default function UserList({ users, action }: UserListProps) {
   return (
     <div className="flex h-full w-full flex-col">
-      {users.map((user) => (
+      {users.slice(0, NUM_TO_SHOW).map((user) => (
         <UserListEntry key={user.id} user={user} action={action} />
       ))}
+
+      {users.length === 0 && (
+        <h2 className="w-full p-2 text-center text-xl">Nothing here...</h2>
+      )}
+
+      {users.length > NUM_TO_SHOW && (
+        <h2 className="w-full p-2 text-center">{`+ ${users.length - NUM_TO_SHOW} More`}</h2>
+      )}
     </div>
   )
 }
