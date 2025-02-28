9ed7f7f335a27206dbb7a2c92211d509d5452b46
benjrkeith <37029865+benjrkeith@users.noreply.github.com>
2025-02-28 12:09:38 +0000

    Update zustand store to support new hook useUserList and tracking chat read status

--- client/src/types/store.ts
+++ client/src/types/store.ts
@@ -4,16 +4,20 @@ import { User } from '@/types/user'
 
 type State = {
   user: User | null
+  users: User[]
   chats: Map<string, Chat>
 }
 
 type Actions = {
   setUser: (user: User) => void
 
+  setUsers: (users: User[]) => void
+
   addChat: (chat: Chat) => void
   setChats: (chats: Chat[]) => void
 
   setFetched: (chat_id: string) => void
+  setRead: (chat_id: string) => void
 
   addMessage: (message: Message) => void
   addMessages: (chat_id: string, messages: Message[]) => void
